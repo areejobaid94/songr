352206ba5d2ad7148cb22bcf4ded147ab7aef391
areejobaid94 <areej.obaid@yahoo.com>
2021-04-20 21:09:32 +0300

    done

--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.songr;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@WebMvcTest
 class SongrApplicationTests {
-
-	@Test
-	void contextLoads() {
+	@Autowired
+	MockMvc mockMvc;
+	@Test void contextLoads() {
 	}
 
 }
